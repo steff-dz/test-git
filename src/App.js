a04194f835d7238c9c5404a6464154a7b1888bb9
Steff <stefaniemarie@gmail.com>
2021-01-13 11:57:56 +0100

    changed text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <>
-      <h1>Hallo verden!</h1>
+      <h1>New Name</h1>
       <p>
         Jeg er en veldig enkel React-app som du kan endre og leke med som du vil.
       </p>
